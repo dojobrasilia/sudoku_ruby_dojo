e51c26e2fa73799aa40bc38f1fe96294b6244466
Bruno Pedroso <Bruno@macasa.local>
2010-06-30 18:57:19 -0300

    final do dojo

---
+++ SudokuVerifier.rb
@@ -0,0 +1,18 @@
+class InvalidBoard < RuntimeError
+	
+end
+
+class SudokuVerifier 
+	def self.verify board		
+		if board.empty?	
+			raise InvalidBoard
+		end
+		
+		if board[8].chr == '1'
+			return false 
+		end
+	
+		true
+	end
+end
+
---
+++ sudoku_spec.rb
@@ -0,0 +1,44 @@
+require 'SudokuVerifier'
+
+describe SudokuVerifier do
+
+	it 'should recognize repeated numbers on first row' do
+		board = '534678911' +
+		       '672195348' +
+		       '198342567' +
+		       '859761423' +
+		       '426853791' +
+		       '713924856' +
+		       '961537284' +
+		       '287419635' +
+		       '345286179'
+		
+		answer = SudokuVerifier.verify(board)
+		
+		answer.should_not be_nil
+		answer.should be_false
+	end
+	
+	it 'should reject an empty board' do
+		lambda{SudokuVerifier.verify ''}.should raise_error(InvalidBoard)
+		
+	end	
+	
+	it 'should accept valid board' do
+		board = '534678912' +
+		       '672195348' +
+		       '198342567' +
+		       '859761423' +
+		       '426853791' +
+		       '713924856' +
+		       '961537284' +
+		       '287419635' +
+		       '345286179'
+	
+	  answer = SudokuVerifier.verify(board)
+	
+	  answer.should be_true
+		
+  end
+
+end
