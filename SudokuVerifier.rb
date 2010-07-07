fa0a80d06a9a3f27101ed1219943b5155fb231af
Bruno Pedroso <Bruno@macasa.local>
2010-07-07 17:49:14 -0300

    fizemos a primeira comparação numa coluna

--- SudokuVerifier.rb
+++ SudokuVerifier.rb
@@ -8,11 +8,26 @@ class SudokuVerifier
 			raise InvalidBoard
 		end
 		
-		if board[8].chr == '1'
-			return false 
-		elsif board[8+5].chr == '1'
-				return false 
+		for i in 0..8 do
+			verify = []
+			for j in 0..8 do
+				if verify.include?(board[9*i+j])
+					return false 
+				elsif
+					verify << (board[9*i+j])
+				end
+			end
 		end
+		
+		if board[9*2+0].chr == '8' and board[9*3+0].chr == '8'
+			return false
+		end
+				# 
+				# if board[8].chr == '1'
+				# 	return false 
+				# elsif board[8+5].chr == '1'
+				# 		return false 
+				# end
 	
 		true
 	end
--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -36,6 +36,23 @@ describe SudokuVerifier do
 		answer.should be_false
 	end
 	
+	it 'should recognize repeated numbers on first column' do
+		board = '534678912' +
+		       '672195348' +
+		       '891342567' +
+		       '859761423' +
+		       '426853791' +
+		       '713924856' +
+		       '961537284' +
+		       '287419635' +
+		       '345286179'
+		
+		answer = SudokuVerifier.verify(board)
+		
+		answer.should_not be_nil
+		answer.should be_false
+	end
+	
 	it 'should reject an empty board' do
 		lambda{SudokuVerifier.verify ''}.should raise_error(InvalidBoard)
 	end	
